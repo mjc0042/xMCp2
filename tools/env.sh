f724b533206586f040cdf3d9b0d7ce94762a0165
Mike <mjc0042@gmail.com>
2018-11-09 19:25:07 -0500

    Switching database to Postgre-sql.

---
+++ tools/env.sh
@@ -0,0 +1,7 @@
+export VENV='/home/mike/workspace/venv'
+
+# Web Server Commands
+alias npmrun='npm run dev'
+alias webstart='python manage.py runserver'
+
+source $VENV/bin/activate
--- xMCp2/settings.py
+++ xMCp2/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'xMCp2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'xmcp2',
+        'USER': 'xmcp2',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': ''
     }
 }
 
