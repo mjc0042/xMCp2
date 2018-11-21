3df7585f28d91444b0c4594a9fcd19e65dbf1328
Mike <mjc0042@gmail.com>
2018-11-20 20:16:23 -0500

    Adding shell script to set up environment.

--- tools/env.sh
+++ tools/env.sh
@@ -1,7 +1,21 @@
+#!/bin/bash
+
 export VENV='/home/mike/workspace/venv'
 
 # Web Server Commands
 alias npmrun='npm run dev'
 alias webstart='python manage.py runserver'
+
+# Django Database Migration Commands
+alias makemigrations='python manage.py makemigrations'
+alias migrate='python manage.py migrate'
+alias showmigrations='python manage.py showmigrations'
+
+# Database
+alias enterdb='sudo -i -u postgres'
+alias xmcp2db='sudo -i -u postgres psql -d xmcp2'
+
+# Log Directory
+export LOG_DIR='/home/mike/workspace/xMCp2/logs'
 
 source $VENV/bin/activate
